96d0d39e4c8fc73f5ea6a3a82380013a11f6428d
Duriamr <Duriamr@outlook.com>
2021-02-17 15:39:30 +0800

    订单页80%

--- pages/tabBar/order/order.vue
+++ pages/tabBar/order/order.vue
@@ -31,94 +31,47 @@
 			<view class="ul">
 				<view class="li">
 					<text>订单:</text>
-					<text class="number">1</text>
+					<text class="number">{{totalCount}}</text>
 					<text>件</text>
 				</view>
 				<view class="li">
 					<text>总销售额:</text>
-					<text class="number">648654657</text>
+					<text class="number">{{totalSaleAmount}}</text>
 					<text>元</text>
 				</view>
 				<view class="li">
 					<text>净利润:</text>
-					<text class="number">416847878</text>
+					<text class="number">{{totalGrossProfits}}</text>
 					<text>元</text>
 				</view>
 			</view>
 		</view>
 		<!-- 销售单 -->
-		<view class="order-sell" v-if="navIndex==1">
-			<view class="content">
-				<view class="img">
-					<image src="../../../static/addGoods/add.png"></image>
-				</view>
-				<view class="details">
-					<view class="title">
-						<text style="margin-left: -10rpx;">【99新】GUCCI 迷你手提包</text>
+		<view class="order_ul">
+			<view class="order_li" v-for="(item,index) in orderArr" :key="index">
+				<view class="right">
+					<view class="img">
+						<image :src="item.mainItem.imageUrl" lazy-load></image>
 					</view>
-					<text>销售时间:2021-02-02 15:13</text>
-					<text>销售员工:莉莉卡</text>
-					<text>订单客户:薇薇安</text>
+					<view class="details">
+						<view class="title">
+							<text v-if="item.mainItem.qualityInfo" style="margin-left: -10rpx;">【{{item.mainItem.qualityInfo}}】</text>{{item.mainItem.name}} 
+						</view>
+						<text>销售时间:{{item.orderTime}}</text>
+						<text>销售员工:{{item.operatorName}}</text>
+						<text>订单客户:{{item.customerName}}</text>
+					</view>
 				</view>
 				<view class="state">
 					<view class="top">
-						<text>已销售</text>
-					</view>
-					<view class="centre">
-						<text class="symbol">¥</text>
-						<text>10000</text>
-					</view>
-					<view class="bot">
-						<text>实收款</text>
-					</view>
-				</view>
-			</view>
-		</view>
-		<!-- 预定单 -->
-		<view class="order-reserve" v-if="navIndex==2">
-			<view class="content">
-				<view class="img">
-					<image src="../../../static/addGoods/add.png"></image>
-				</view>
-				<view class="details">
-					<view class="title">
-						<text style="margin-left: -10rpx;">【99新】GUCCI 迷你手提包</text>
-					</view>
-					<text>销售时间:2021-02-02 15:13</text>
-					<text>销售员工:莉莉卡</text>
-					<text>订单客户:薇薇安</text>
-				</view>
-				<view class="state">
-					<view class="top">
-						<text>已预定</text>
-					</view>
-					<view class="centre">
-						<text class="symbol">¥</text>
-						<text>10000</text>
-					</view>
-					<view class="bot">
-						<text>实收款</text>
-					</view>
-				</view>
-			</view>
-		</view>
-		<!-- 退货单 -->
-		<view class="order-cancel" v-if="navIndex==3">
-			<view class="content">
-				<view class="img">
-					<image src="../../../static/addGoods/add.png"></image>
-				</view>
-				<view class="details">
-					<view class="title">
-						<text style="margin-left: -10rpx;">【99新】GUCCI 迷你手提包</text>
-					</view>
-					<text>销售时间:2021-02-02 15:13</text>
-					<text>销售员工:莉莉卡</text>
-					<text>订单客户:薇薇安</text>
-				</view>
-				<view class="state">
-					<view class="top">
-						<text>已退货</text>
+						<text v-if="item.orderStatus==1">未确认</text>
+						<text v-if="item.orderStatus==2&&navIndex!=2" style="color: #57BFA3;">已销售</text>
+						<text v-if="item.orderStatus==3&&navIndex!=2">已取消</text>
+						<text v-if="item.orderStatus==2&&navIndex==2" style="color: #EFA22A;">已预定</text>
+						<text v-if="item.orderStatus==3&&navIndex==2">预定取消</text>
+						<text v-if="item.orderStatus==4">无效订单</text>
+						<text v-if="item.orderStatus==5">已退货</text>
+						<text v-if="item.orderStatus==6">已关闭</text>
 					</view>
 					<view class="centre">
 						<text class="symbol">¥</text>
@@ -137,37 +90,152 @@
 	export default {
 		data() {
 			return {
-				navIndex: 1,//导航栏index
-				//发送请求页码
+				navIndex: 0,//导航栏index
+				
+				//发送请求
+				//页码
 				pageNo: 1,
 				pageSize: 10,
-				
+				//搜索数据
 				searchText: '', //搜索文本
+				orderTypeList:null, // 订单类型列表 (列表中各项为整型: 0-订单,1-预订单,此项值为null时为不区分订单和预订单),此项传null值时为不限制
+				orderStatus:null, // 订单状态列表 (列表中各项为整型: 1-未确认,2-已确认,3-已取消,4-无效,5-退货,6-关闭),此项传null值时为不限制
+				operatorIdList: null, // 经办人id列表 (列表中各项长整型，在js中最好转为字符再传递，辟免丢失精度),此项传null值时为不限制
+				orderDateStart:null, // 查询下单范围的起始时间(格式：yyyy-MM-dd，如2020-11-01)
+				orderDateEdn:null,// 查询下单范围的结束时间(格式：yyyy-MM-dd，如2020-11-07)
+				
+				totalCount:'',
+				totalSaleAmount:'',
+				totalGrossProfits:'',
+				orderArr:[],
 			};
 		},
 		onLoad(){
-			this.getShopOrderArr()
+			this.tabPagination(1)
+		},
+		onPullDownRefresh(){
+			if(this.navIndex==1){
+				this.orderTypeList = [0]
+				this.orderStatus = null
+				this.getOrderArr()
+			}else if(this.navIndex==2){
+				this.orderTypeList = [1]
+				this.orderStatus = null
+				this.getOrderArr()
+			}else if(this.navIndex==3){
+				this.orderTypeList = null
+				this.orderStatus = [5]
+				this.getOrderArr()
+			}
+		},
+		onReachBottom(){
+			this.addOrderArr()
 		},
 		methods:{
+			tabPagination(index) {
+				if(this.navIndex == index){
+					return
+				}
+				this.navIndex = index
+				this.orderArr = []
+				this.totalCount = ''
+				this.totalSaleAmount = ''
+				this.totalGrossProfits =''
+				uni.startPullDownRefresh()
+				console.log(index)
+			},
 			//获取订单详情数据
-			async getShopOrderArr(){
-				uni.showLoading()
+			async getOrderArr(){
+				// uni.showLoading()
 				let params = {
-					sort: "",
-					searchText: this.searchText,
 					pageNo: 1,
 					pageSize: this.pageSize,
+					// searchText: this.searchText,
+					keyText: this.searchText, 
+					orderTypeList:this.orderTypeList,
+					orderStatus:this.orderStatus,
+					operatorIdList: this.operatorIdList,
+					orderDateStart:this.orderDateStart,
+					orderDateEdn:this.orderDateEdn,
+				};
+				let res = await this.$post({
+					url:'/shopOrder/list',
+					data:params,
+				})
+				// uni.hideLoading()
+				uni.stopPullDownRefresh()
+				console.log(res.data)
+				let data = res.data.data
+				this.totalCount = data.totalCount
+				this.totalSaleAmount = data.totalSaleAmount,
+				this.totalGrossProfits = data.totalGrossProfits,
+				uni.pageScrollTo({
+				    scrollTop: 0,
+				    duration: 100
+				});
+				for(let item of data.pageData.records){
+					if(item.items.length>0&&item.items[0].goodsSku){
+						item.mainItem = item.items[0].goodsSku
+						if (item.mainItem.name.length > 24) {
+						    item.mainItem.name = item.mainItem.name.substring(0, 21) + '...';
+						}
+						item.mainItem.imageUrl = this.$imgUrl+item.items[0].goodsSku.picList[0].imagePath
+					}else{
+						iitem.mainItem = {
+							name:'',
+							imageUrl:'',
+							qualityInfo:'',
+						}
+					}
+				}
+				console.log(data.pageData.records)
+				this.orderArr = data.pageData.records
+			},
+			//下滑订单详情数据
+			async addOrderArr(){
+				uni.showLoading()
+				let params = {
+					pageNo: ++this.pageNo,
+					pageSize: this.pageSize,
+					// searchText: this.searchText,
+					keyText: this.searchText, 
+					orderTypeList:this.orderTypeList,
+					orderStatus:this.orderStatus,
+					operatorIdList: this.operatorIdList,
+					orderDateStart:this.orderDateStart,
+					orderDateEdn:this.orderDateEdn,
 				};
 				let res = await this.$post({
 					url:'/shopOrder/list',
 					data:params,
 				})
 				uni.hideLoading()
-				console.log(res.data)
-				// this.shopOrderArr = res.data.data
-			},
-			tabPagination(index) {
-				this.navIndex = index
+				let data = res.data.data
+				if(data.pageData.records.length>0){
+					for(let item of data.pageData.records){
+						if(item.items.length>0&&item.items[0].goodsSku){
+							item.mainItem = item.items[0].goodsSku
+							if (item.mainItem.name.length > 24) {
+							    item.mainItem.name = item.mainItem.name.substring(0, 21) + '...';
+							}
+							item.mainItem.imageUrl = this.$imgUrl+item.items[0].goodsSku.picList[0].imagePath
+						}else{
+							iitem.mainItem = {
+								name:'',
+								imageUrl:'',
+								qualityInfo:'',
+							}
+						}
+						this.orderArr.push(item)
+					}
+				}else{
+					--this.pageNo
+					uni.showToast({
+						title:'没有更多了',
+						icon:'none',
+						position:'bottom'
+					})
+				}
 			},
 		}
 	}
