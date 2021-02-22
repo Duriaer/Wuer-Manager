9ba5c4a739edde169c3d29468bf481c078c3a373
Duriaer <Duriaer@outlook.com>
2021-02-22 16:04:53 +0800

    新增角色5%

--- pages/index/addRole/addRole.vue
+++ pages/index/addRole/addRole.vue
@@ -1,12 +1,22 @@
 <template>
 	<view class="addRole">
+		<view class="message">
 			<view class="ul">
-				<view class="line_input" style="margin-top: 16rpx;">
+				<view class="line_input">
 					<view class="line_name">
 						<text>角色名称</text><text class="star">*</text>
 					</view>
 					<view class="right">
 						<input type="text" placeholder="请输入" />
+					</view>
+				</view>
+				<view class="line_textarea">
+					<view class="line_name">
+						<text>备注</text>
+					</view>
+					<view class="textarea">
+						<textarea placeholder="请输入" maxlength="100" />
+						<text class="count">0/100</text>
 					</view>
 				</view>
 				<view class="line_switch">
@@ -19,6 +29,28 @@
 					</view>
 				</view>
 			</view>
+		</view>
+		<text class="title">权限列表</text>
+		<view class="authority">
+			<view class="title">
+				<text>基础</text>
+			</view>
+		</view>
+		<view class="authority">
+			<view class="title">
+				<text>商品</text>
+			</view>
+		</view>
+		<view class="authority">
+			<view class="title">
+				<text>销售</text>
+			</view>
+		</view>
+		<view class="authority">
+			<view class="title">
+				<text>客户</text>
+			</view>
+		</view>
 	</view>
 </template>
 
